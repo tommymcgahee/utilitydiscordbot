9bb59ba45ba08846846fb9a05a55190895de60cd
Tommy McGahee <tommy.mcgahee@gmail.com>
2020-10-06 16:23:40 -0400

    First commit.

---
+++ README.md
@@ -0,0 +1,29 @@
+# A utility Discord bot for servers
+
+![](https://github.com/tommymcgahee/utilitydiscordbot/workflows/Node/badge.svg)
+
+This is a general use discord bot to manage aspects of a Discord server.
+
+## How it works
+
+*Currently only supports changing nicknames* 
+
+Use `!changenick @user <new nickname>` in any channel / bot DM. Name changes will be announced as an embed in the channel provided in [config.json](https://github.com/tommymcgahee/utilitydiscordbot/blob/master/config.example.json#L5). 
+
+Additional commands should also be added to [config.json](https://github.com/tommymcgahee/utilitydiscordbot/blob/master/config.example.json#L4). 
+
+Use this [discordjs guide](https://discordjs.guide/preparations/setting-up-a-bot-application.html) to register and configure the bot inside Discord's developer portal. 
+
+## Requirements
+
+Currently compatible with [Node.js 10, 12 and 14](https://github.com/tommymcgahee/utilitydiscordbot/actions?query=workflow%3ANode). 
+
+## Docker Requirements
+
+`Docker Compose` is required. 
+
+To run nodemon and set `NODE_ENV` to development:  
+`docker-compose -f docker-compose.yml -f development.yml up`
+
+To build for production:  
+`docker-compose up`
---
+++ index.js
@@ -0,0 +1,71 @@
+const fs = require('fs'); 
+const { prefix, token, commands, channel } = require('./config.json');
+
+const { Client, MessageEmbed } = require('discord.js');
+const client = new Client(); 
+
+const escapeRegex = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+const getUserFromMention = mention => {
+	if (!mention) return;
+
+	if (mention.startsWith('<@') && mention.endsWith('>')) {
+		mention = mention.slice(2, -1);
+
+		if (mention.startsWith('!')) {
+			mention = mention.slice(1);
+		}
+
+		return client.users.cache.get(mention);
+	}
+}
+
+client.once('ready', () => {
+	console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
+  	client.user.setActivity(`${client.guilds.cache.size} House(s)`, { type: 'WATCHING' });
+});
+
+client.on('message', async message => {
+	const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
+	if (!prefixRegex.test(message.content) || message.author.bot) return;
+	
+	const [, matchedPrefix] = message.content.match(prefixRegex);
+	const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!commands.includes(command)) return;
+
+	if (command === 'changenick') {
+		if (args[0]) {
+			const mentionedUser = getUserFromMention(args[0]);
+			if (!mentionedUser) {
+				return message.reply('Please use a proper mention if you want to set a nickname.');
+			}
+			const newNickname = args.slice(1).join(" ");
+			message.guild.members.cache.find(user => user.id === mentionedUser.id).setNickname(newNickname);
+			const embed = new MessageEmbed()
+			.setTitle('ATTN: A nickname has been changed')
+			.setColor(0xff0000)
+			.setDescription(`I, <@!${client.user.id}>, have allowed <@!${message.author.id}> to change <@!${mentionedUser.id}>'s nickname to ${args[1]}.`);
+			// Send the embed to the same channel as the message
+			client.channels.cache.get(channel).send(embed);
+		}
+	}
+});
+
+client.login(token);
+
+// Docker graceful exit commands
+const process = require('process');
+
+process.on('SIGINT', function onSigint() {
+	client.shutdown();
+});
+
+process.on('SIGTERM', function onSigterm() {
+	client.shutdown();
+});
+
+client.shutdown = function () {
+	// clean up your resources and exit 
+	process.exit();
+};
